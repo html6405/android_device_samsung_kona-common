5b050c0455bac79374626811095b78b19a7c4989
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-04-26 13:50:03 +0200

    Update init program.

--- init/init_kona.cpp
+++ init/init_kona.cpp
@@ -17,27 +17,32 @@ using android::base::Trim;
 
 #define SERIAL_NUMBER_FILE "/efs/FactoryApp/serial_no"
 
-void property_override(char const prop[], char const value[])
-{
-    prop_info *pi;
+// copied from build/tools/releasetools/ota_from_target_files.py
+// but with "." at the end and empty entry
+std::vector<std::string> ro_product_props_default_source_order = {
+    "",
+    "product.",
+    "product_services.",
+    "odm.",
+    "vendor.",
+    "system.",
+    "system_ext.",
+};
 
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
+void property_override(char const prop[], char const value[], bool add = true)
+{
+    auto pi = (prop_info *) __system_property_find(prop);
+
+    if (pi != nullptr) {
         __system_property_update(pi, value, strlen(value));
-    else
+    } else if (add) {
         __system_property_add(prop, strlen(prop), value, strlen(value));
-}
-
-void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
-{
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
+    }
 }
 
 void vendor_load_properties()
 {
     const std::string bootloader = GetProperty("ro.bootloader", "");
-    const std::string platform = GetProperty("ro.board.platform", "");
 
     char const *serial_number_file = SERIAL_NUMBER_FILE;
     std::string serial_number;
@@ -47,37 +52,53 @@ void vendor_load_properties()
         property_override("ro.serialno", serial_number.c_str());
     }
 
-    if (bootloader.find("N5100") != std::string::npos) {
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "GT-N5100");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "kona3g");
-        property_override_dual("ro.product.device", "ro.vendor.product.name", "kona3gxx");
-        property_override_dual("ro.build.description", "ro.vendor.build.description", "kona3gxx-user 4.4.2 KOT49H N5100XXSDQA2 release-keys");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/kona3gxx/kona3g:4.4.2/KOT49H/N5100XXSDQA2:user/release-keys");
+    const auto set_ro_product_prop = [](const std::string &source,
+            const std::string &prop, const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    if (bootloader.find("N5100") == 0) {
+        /* jfltetmo */
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_product_prop(source, "fingerprint", "samsung/kona3gxx/kona3g:4.4.2/KOT49H/N5100XXSDQA2:user/release-keys");
+            set_ro_product_prop(source, "device", "kona3g");
+            set_ro_product_prop(source, "model", "GT-N5100");
+            set_ro_product_prop(source, "name", "kona3gxx");
+        }
+        property_override("ro.build.description", "kona3gxx-user 4.4.2 KOT49H N5100XXSDQA2 release-keys");
         property_override("ro.build.product", "n5100");
-    } else if (bootloader.find("N5110") != std::string::npos) {
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "GT-N5110");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "konawifi");
-        property_override_dual("ro.product.name", "ro.vendor.product.name", "konawifixx");
-        property_override_dual("ro.build.description", "ro.vendor.build.description", "konawifixx-user 4.4.2 KOT49H N5110XXDNF1 release-keys");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/konawifixx/konawifi:4.4.2/KOT49H/N5110XXDNF1:user/release-keys");
+    } else if (bootloader.find("N5110") == 0) {
+        /* jgedlte */
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_product_prop(source, "fingerprint", "samsung/konawifixx/konawifi:4.4.2/KOT49H/N5110XXDNF1:user/release-keys");
+            set_ro_product_prop(source, "device", "konawifi");
+            set_ro_product_prop(source, "model", "GT-N5110");
+            set_ro_product_prop(source, "name", "konawifixx");
+        }
+        property_override("ro.build.description", "konawifixx-user 4.4.2 KOT49H N5110XXDNF1 release-keys");
         property_override("ro.build.product", "n5110");
-    } else if (bootloader.find("N5120") != std::string::npos) {
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "GT-N5120");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "konalte");
-        property_override_dual("ro.product.name", "ro.vendor.product.name", "konaltexx");
-        property_override_dual("ro.build.description", "ro.vendor.build.description", "konaltexx-user 4.4.2 KOT49H N5120XXDOD1 release-keys");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/konaltexx/konalte:4.4.2/KOT49H/N5120XXDOD1:user/release-keys");
+    } else if (bootloader.find("N5120") == 0) {
+        /* jfltexx */
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_product_prop(source, "fingerprint", "samsung/konaltexx/konalte:4.4.2/KOT49H/N5120XXDOD1:user/release-keys");
+            set_ro_product_prop(source, "device", "konalte");
+            set_ro_product_prop(source, "model", "GT-N5120");
+            set_ro_product_prop(source, "name", "konaltexx");
+        }
+        property_override("ro.build.description", "konaltexx-user 4.4.2 KOT49H N5120XXDOD1 release-keys");
         property_override("ro.build.product", "n5120");
     } else {
-        /* Fall back to GT-N5110 (Wifi version) */
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "GT-N5110");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "konawifi");
-        property_override_dual("ro.product.name", "ro.vendor.product.name", "konawifixx");
-        property_override_dual("ro.build.description", "ro.vendor.build.description", "konawifixx-user 4.4.2 KOT49H N5110XXDNF1 release-keys");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/konawifixx/konawifi:4.4.2/KOT49H/N5110XXDNF1:user/release-keys");
+        /* jfltevzw */
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_product_prop(source, "fingerprint", "samsung/kona3gxx/kona3g:4.4.2/KOT49H/N5100XXSDQA2:user/release-keys");
+            set_ro_product_prop(source, "device", "kona3g");
+            set_ro_product_prop(source, "model", "GT-N5100");
+            set_ro_product_prop(source, "name", "kona3gxx");
+        }
+        property_override("ro.build.description", "konawifixx-user 4.4.2 KOT49H N5110XXDNF1 release-keys");
         property_override("ro.build.product", "n5110");
     }
-
     const std::string device = GetProperty("ro.product.device", "");
     LOG(INFO) << "Found bootloader " << bootloader << ". " << "Setting build properties for " << device << ".\n";
 }
