4ca83f4600dfef574044e3fd089519b7c6e94fd1
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-09-10 15:00:45 +0200

    Revert "kona : force wideband audio"
    This reverts commit 7f035cfa

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -3096,7 +3096,8 @@ static int adev_open(const hw_module_t* module, const char* name,
     /* RIL */
     ril_open(&adev->ril);
     pthread_mutex_unlock(&adev->lock);
-    adev->wb_amr = true;
+    /* register callback for wideband AMR setting */
+    ril_register_set_wb_amr_callback(audio_set_wb_amr_callback, (void *)adev);
 
     *device = &adev->hw_device.common;
 
