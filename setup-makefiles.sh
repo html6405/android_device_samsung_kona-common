4803a1a9ad19a5d34f841fcd41a88def1c8d9cc9
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-09-15 10:27:59 +0200

    kona-common: update extract-files.sh and setup-makefiles.sh from vendor/lineage/build/templates.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+# Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,6 +17,32 @@
 #
 
 set -e
-export DEVICE=n51xx
-export VENDOR=samsung
-./../smdk4412-common/setup-makefiles.sh $@
+
+DEVICE=kona-common
+VENDOR=kona-common
+
+INITIAL_COPYRIGHT_YEAR=2021
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+write_makefiles "$MY_DIR"/proprietary-files.txt
+
+# Finish
+write_footers
