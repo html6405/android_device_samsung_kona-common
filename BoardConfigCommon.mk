4d4aff43eb15107511b5c2e542485aaec018dc15
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-02-18 18:48:01 +0100

    kona-common: include: use own device specific header files, especially for gps.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,6 +15,8 @@
 #
 include device/samsung/smdk4412-common/BoardCommonConfig.mk
 
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/kona-common/include
+
 WITHOUT_CAMERA2 := true
 WITHOUT_GCAM := false
 
