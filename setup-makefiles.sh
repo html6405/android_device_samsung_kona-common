88c93a57265f37b50210c1621692b84b1c370afe
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-11-03 13:42:44 +0100

    kona-common: setup-makefiles.sh & extract-files.sh: revert changes and update path for lineage

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -16,6 +16,41 @@
 #
 
 set -e
-export DEVICE=n51xx
-export VENDOR=samsung
-./../smdk4412-common/setup-makefiles.sh $@
+DEVICE_COMMON=kona-common
+DEVICE=${DEVICE:-nodevice}
+VENDOR=samsung
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+ANDROID_ROOT="$MY_DIR"/../../..
+
+HELPER="$ANDROID_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper for common device
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$ANDROID_ROOT" true
+
+# Copyright headers and common guards
+write_headers "n5100 n5110 n5120"
+
+write_makefiles "$MY_DIR"/proprietary-files.txt
+
+write_footers
+
+# Reinitialize the helper for the device-specific blobs
+setup_vendor "$DEVICE" "$VENDOR" "$ANDROID_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+# The device-specific blobs
+write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files.txt
+
+# Done
+write_footers
