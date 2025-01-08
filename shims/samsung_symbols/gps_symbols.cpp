a99f6abdf994c8a32e3cee8e254d33f691faffbe
html6405 <peter.schelchshorn@mhs-solutions.at>
2025-01-08 15:44:37 +0100

    kona-common: gps_symbols.cpp: create missing symbols for gpsd

--- shims/samsung_symbols/gps_symbols.cpp
+++ shims/samsung_symbols/gps_symbols.cpp
@@ -23,6 +23,48 @@ extern "C" void _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(){}
 extern "C" void _ZN7android13SensorManager16createEventQueueENS_7String8Ei();
 extern "C" void _ZN7android13SensorManager16createEventQueueENS_7String8Ei(){}
 
+extern "C" void _ZN7android16SensorEventQueue4readEP12ASensorEventj();
+extern "C" void _ZN7android16SensorEventQueue4readEP12ASensorEventj(){}
+
+extern "C" void _ZNK7android16SensorEventQueue5getFdEv();
+extern "C" void _ZNK7android16SensorEventQueue5getFdEv(){}
+
+extern "C" void _ZN7android13SensorManager16getDefaultSensorEi();
+extern "C" void _ZN7android13SensorManager16getDefaultSensorEi(){}
+
+extern "C" void _ZNK7android6Sensor7getNameEv();
+extern "C" void _ZNK7android6Sensor7getNameEv(){}
+
+extern "C" void _ZNK7android16SensorEventQueue12enableSensorEPKNS_6SensorE();
+extern "C" void _ZNK7android16SensorEventQueue12enableSensorEPKNS_6SensorE(){}
+
+extern "C" void _ZNK7android16SensorEventQueue12setEventRateEPKNS_6SensorEx();
+extern "C" void _ZNK7android16SensorEventQueue12setEventRateEPKNS_6SensorEx(){}
+
+extern "C" void _ZNK7android16SensorEventQueue13disableSensorEPKNS_6SensorE();
+extern "C" void _ZNK7android16SensorEventQueue13disableSensorEPKNS_6SensorE(){}
+
+extern "C" void _ZN7android8String16aSEOS0_();
+extern "C" void _ZN7android8String16aSEOS0_(){}
+
+extern "C" void _ZN7android8String16C1EOS0_();
+extern "C" void _ZN7android8String16C1EOS0_(){}
+
+extern "C" void _ZN7android9SingletonINS_13SensorManagerEE5sLockE();
+extern "C" void _ZN7android9SingletonINS_13SensorManagerEE5sLockE(){}
+
+extern "C" void _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE();
+extern "C" void _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE(){}
+
+extern "C" void _ZN7android13SensorManager16createEventQueueEv();
+extern "C" void _ZN7android13SensorManager16createEventQueueEv(){}
+
+extern "C" void _ZN7android13SensorManagerC1Ev();
+extern "C" void _ZN7android13SensorManagerC1Ev(){}
+
+extern "C" void  _ZN7android13SensorManagerC1ERKNS_8String16E();
+extern "C" void _ZN7android13SensorManagerC1ERKNS_8String16E(){}
+
 extern "C" {
 
 const SSL_METHOD *SSLv3_method(void) {
